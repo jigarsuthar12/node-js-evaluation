bc314882dcf5f0119fbaa65b7ed02f37584c58b1
jigarsuthar12 <jigar.suthar@seaflux.tech>
2024-03-19 18:14:04 +0530

    fix: updated code without passing any token

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { UserEntity } from "@entities";
-import { Bcrypt, InitRepository, InjectRepositories, JwtHelper } from "@helpers";
+import { Bcrypt, InitRepository, InjectRepositories } from "@helpers";
 import { TRequest, TResponse } from "@types";
 import { Repository } from "typeorm";
 import { CreateUserDto } from "./dto";
@@ -16,8 +16,7 @@ export class AuthController {
     req.dto.password = await Bcrypt.hash(req.dto.password);
     const user = await this.userRepository.create(req.dto);
     await this.userRepository.save(user);
-    const token = JwtHelper.encode({ id: user.id });
 
-    return res.status(200).json({ msg: "USER_CREATED", token });
+    return res.status(200).json({ msg: "USER_CREATED" });
   };
 }
