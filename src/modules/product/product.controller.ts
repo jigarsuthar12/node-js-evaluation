6e027b5516bd9b53734c8a32ac843d00e1e8a783
jigarsuthar12 <jigar.suthar@seaflux.tech>
2024-03-26 12:31:11 +0530

    feat: added listing products functionality

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,9 +1,31 @@
 import { ProductEntity } from "@entities";
 import { InitRepository, InjectRepositories } from "@helpers";
-import { TRequest, TResponse } from "@types";
-import { Repository } from "typeorm";
+import { Category, TRequest, TResponse } from "@types";
+import { Between, FindOperator, LessThanOrEqual, Like, MoreThanOrEqual, Repository } from "typeorm";
 import { CreateProductDto, UpdateProductDto } from "./dto";
 
+interface ProductQueryParams {
+  allProducts?: string;
+  name?: string;
+  category?: Category;
+  minPrice?: number;
+  maxPrice?: number;
+  rating?: number;
+  discount?: string;
+  sortBy?: "priceLowToHigh" | "priceHighToLow" | "name";
+}
+
+type WhereClause<T> = Partial<{
+  [K in keyof T]: T[K] | FindOperator<T[K]>;
+}>;
+
+type Product = {
+  category?: Category;
+  name?: string;
+  price?: number;
+  rating?: number;
+  discount?: string;
+};
 export class ProductController {
   @InitRepository(ProductEntity)
   productRepository: Repository<ProductEntity>;
@@ -11,6 +33,62 @@ export class ProductController {
   constructor() {
     InjectRepositories(this);
   }
+
+  public getTrendingList = async (req: TRequest, res: TResponse) => {
+    const products = await this.productRepository.find({
+      order: {
+        id: "DESC",
+      },
+      take: 5,
+    });
+
+    return res.status(200).json({ msg: "TRENDING ITEMS", products });
+  };
+
+  public get = async (req: TRequest, res: TResponse) => {
+    const { allProducts, name, category, minPrice, maxPrice, sortBy, discount } = req.query as ProductQueryParams;
+
+    const where: WhereClause<Product> = {};
+    if (allProducts === "true") {
+      const products = await this.productRepository.find();
+      return res.status(200).json({ msg: "ALL PRODUCTS", products });
+    }
+    if (category && name) {
+      where.category = category;
+      where.name = Like(`%${name}%`);
+    }
+    if (category) {
+      where.category = category;
+    }
+    if (name) {
+      where.name = Like(`%${name}%`);
+    }
+
+    const order: { [key: string]: "ASC" | "DESC" } = {};
+    if (sortBy === "priceLowToHigh") {
+      order.price = "ASC";
+    }
+    if (sortBy === "priceHighToLow") {
+      order.price = "DESC";
+    }
+    if (sortBy === "name") {
+      order.name = "ASC";
+    }
+    if (minPrice && maxPrice) {
+      where.price = Between(minPrice, maxPrice);
+    }
+    if (minPrice) {
+      where.price = MoreThanOrEqual(minPrice);
+    }
+    if (maxPrice) {
+      where.price = LessThanOrEqual(maxPrice);
+    }
+    if (discount) {
+      where.discount = discount;
+    }
+    const products = await this.productRepository.find({ where, order });
+    return res.status(200).json({ msg: "ALL FILTERED PRODUCTS", products });
+  };
 
   public create = async (req: TRequest<CreateProductDto>, res: TResponse) => {
     const { name, price, imageUrl, description, discount, category } = req.dto as CreateProductDto;
--- src/modules/product/product.route.ts
+++ src/modules/product/product.route.ts
@@ -14,5 +14,7 @@ export class ProductRouter extends SFRouter implements RouterDelegates {
   initRoutes(): void {
     this.router.post("/create-product", Validator.validate(CreateProductDto), this.authMiddleware.auth, this.authMiddleware.isAdmin, this.productController.create);
     this.router.put("/update-product/:productId", Validator.validate(UpdateProductDto), this.authMiddleware.auth, this.authMiddleware.isAdmin, this.productController.update);
+    this.router.get("/", this.productController.get);
+    this.router.get("/get-trending-list", this.productController.getTrendingList);
   }
 }
