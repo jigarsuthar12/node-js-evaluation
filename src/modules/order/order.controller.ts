67b310aca34c31efec9ce971857224846e2d41e7
jigarsuthar12 <jigar.suthar@seaflux.tech>
2024-03-27 15:56:25 +0530

    feat: added access past orders

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -1,3 +1,4 @@
+import { ProductEntity, ReviewEntity, UserEntity } from "@entities";
 import { InitRepository, InjectRepositories } from "@helpers";
 import { TRequest, TResponse } from "@types";
 import { OrderEntity } from "entities/order.entity";
@@ -8,12 +9,45 @@ interface ReviewParams {
 }
 
 export class OrderController {
+  @InitRepository(ProductEntity)
+  productRepository: Repository<ProductEntity>;
+
+  @InitRepository(ReviewEntity)
+  reviewRepository: Repository<ReviewEntity>;
+
+  @InitRepository(UserEntity)
+  userRepository: Repository<UserEntity>;
+
   @InitRepository(OrderEntity)
   orderRepository: Repository<OrderEntity>;
 
   constructor() {
     InjectRepositories(this);
   }
+
+  public pastOrder = async (req: TRequest, res: TResponse) => {
+    const orders = await this.orderRepository.find({ where: { userId: req.me.id } });
+    const mappedOrders = await Promise.all(
+      orders.map(async item => {
+        const products = await this.productRepository.find({ where: { id: item.productId } });
+        const mappedProducts = await Promise.all(
+          products.map(async productItem => {
+            const reviews = await this.reviewRepository.find({ where: { productId: productItem.id } });
+            const updatedReviews = await Promise.all(
+              reviews.map(async reviewItem => {
+                const user = await this.userRepository.findOne({ where: { id: reviewItem.userId } });
+                return { ...reviewItem, username: user.name };
+              }),
+            );
+            return { ...item, reviews: updatedReviews };
+          }),
+        );
+        return { ...item, product: mappedProducts };
+      }),
+    );
+    const user = await this.userRepository.findOne({ where: { id: req.me.id } });
+    return res.status(200).json({ msg: "ALL ORDERS", Orders: mappedOrders, username: user.name });
+  };
 
   public placeOrder = async (req: TRequest, res: TResponse) => {
     const { productId } = req.params as ReviewParams;
